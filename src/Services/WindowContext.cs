c6faf796c0eab16a389873eab0c9d40eaf2a8382
Róbert Fuszenecker <fuszenecker@users.noreply.github.com>
2026-05-20 18:11:27 +0200

    Button color and info dialog gets modal. (#11)
    * Button color fix.
    
    * Make info dialog modal.

--- src/Services/WindowContext.cs
+++ src/Services/WindowContext.cs
@@ -1,4 +1,7 @@
+using System.Linq;
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Controls.ApplicationLifetimes;
 using PentaGrammata.Interfaces;
 
 namespace PentaGrammata.Services;
@@ -6,4 +9,19 @@ namespace PentaGrammata.Services;
 public sealed class WindowContext : IWindowContext
 {
     public Window? MainWindow { get; set; }
+
+    public Window? ActiveWindow
+    {
+        get
+        {
+            if (Application.Current?.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+            {
+                return desktop.Windows.FirstOrDefault(w => w.IsActive)
+                    ?? MainWindow
+                    ?? desktop.MainWindow;
+            }
+
+            return MainWindow;
+        }
+    }
 }
---
+++ tests/PentaGrammata.Tests/Services/InfoDialogServiceTests.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using PentaGrammata.Interfaces;
+using PentaGrammata.Services;
+
+namespace PentaGrammata.Tests.Services;
+
+[TestClass]
+public sealed class InfoDialogServiceTests
+{
+    [TestMethod]
+    public void Constructor_NullWindowContext_ThrowsArgumentNullException()
+    {
+        Assert.ThrowsExactly<ArgumentNullException>(() => new InfoDialogService(null!));
+    }
+
+    [TestMethod]
+    public async Task ShowInfoAsync_WhenActiveWindowIsNull_CompletesWithoutShowingDialog()
+    {
+        var windowContext = Substitute.For<IWindowContext>();
+        windowContext.ActiveWindow.Returns((Avalonia.Controls.Window?)null);
+        var sut = new InfoDialogService(windowContext);
+
+        // Must not throw; no Avalonia dialog is created when there is no owner window.
+        await sut.ShowInfoAsync("Title", "Message");
+    }
+}
